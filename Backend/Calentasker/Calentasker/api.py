4b865224f80d68fdc59bfb4376f539ff9c5f08ef
FesetoImre5 <feseto.imre@diak.szbi-pg.hu>
2025-11-18 08:18:21 +0100

    Clean start
    I got lost

--- Backend/Calentasker/Calentasker/api.py
+++
@@ -1,6 +0,0 @@
-from ninja import NinjaAPI
-from user.api import router as user_router
-
-api = NinjaAPI(title="Calentasker API -- Docs", version="1.0.0", description="API for Calentasker")
-
-api.add_router("/user/", user_router)
--- Backend/Calentasker/user/api.py
+++
@@ -1,7 +0,0 @@
-from ninja import Router
-
-router = Router(tags=["user"])
-
-@router.get("/hello")
-def hello(request):
-    return {"message": "API at your service"}
