c0f16d8bb94de093494c207833dbf62ac20b9418
FesetoImre5 <feseto.imre@diak.szbi-pg.hu>
2026-01-21 10:22:31 +0100

    random comment

--- Ends/Backend/Calentasker/users/serializers.py
+++ Ends/Backend/Calentasker/users/serializers.py
@@ -2,12 +2,11 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
-    display_username = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
         fields = ('id', 'username', 'profile_picture', 'first_name', 'last_name', 'email', 'password', 'is_active', 'date_joined')
-        read_only_fields = ('is_active', 'date_joined', 'display_username')
+        read_only_fields = ('is_active', 'date_joined')
         extra_kwargs = {
             'password': {'write_only': True},
             'username': {'required': False, 'allow_null': True}
@@ -18,15 +17,14 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
 class UserSearchSerializer(serializers.ModelSerializer):
-    display_username = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'username', 'display_username', 'first_name', 'last_name', 'profile_picture')
+        fields = ('id', 'email', 'username', 'first_name', 'last_name', 'profile_picture')
 
 class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'display_username', 'profile_picture', 'first_name', 'last_name', 'email', 'password')
+        fields = ('id', 'username', 'profile_picture', 'first_name', 'last_name', 'email', 'password')
         read_only_fields = fields
